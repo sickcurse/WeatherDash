4530d98763a315df2d04dce0ff674393c45a6924
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-09-04 14:21:11 -0400

    building script.js file, included api key and host. sreachHistory is collecting input

---
+++ script.js
@@ -0,0 +1,3 @@
+let searchHistory = JSON.parse(localStorage.getItem('search-history')) || [];
+const weatherUrl = 'https://api.openweathermap.org';
+const weatherApiKey = 'cffa92cd0ca96f94480dd91598059b25';
