ac057a043cc561c94a62e7d2b7df55d006ca6414
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-09-04 14:32:23 -0400

    adding function to see sreach history and interact with them via buttons.

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ const searchInput = document.querySelector('#search-input');
 const today = document.querySelector('#today');
 const forecast= document.querySelector('#forecast');
 const searchHistoryContainer = document.querySelector('#history');
+
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+function renderSearchHistory() {
+    searchHistoryContainer.innerHTML = searchHistory.map(city =>
+      `<button type="button" data-search="${city}">${city}</button>`
+    ).join('');
+  }
