23b9a6bdb20a8d93c35a75a30f0bf3c56f19a287
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-09-04 14:28:54 -0400

    included DOM elements to interact with form, input and weather display.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let searchHistory = JSON.parse(localStorage.getItem('search-history')) || [];
 const weatherUrl = 'https://api.openweathermap.org';
 const weatherApiKey = 'cffa92cd0ca96f94480dd91598059b25';
+
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-input');
+const today = document.querySelector('#today');
+const forecast= document.querySelector('#forecast');
+const searchHistoryContainer = document.querySelector('#history');
